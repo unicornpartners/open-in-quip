647e69f8c00c6972ef9d9a999e07433844d352cf
Matt Y <letters@mattgy.net>
2018-07-20 13:14:42 -0400

    fixed tab.url.match

--- oiq.js
+++ oiq.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
-  if (tab.url.match(/https?:\/\/[A-Za-z0-9]+\.?quip-amazon\.com/) != null) {
+  if (tab.url.match(/https?:\/\/quip-amazon\.com/) != null) {
     var quipId = tab.url.split('/').pop();
     if (quipId.length == 12 || quipId.length == 13) {
       chrome.tabs.update(tab.id, { "url": "quip://" + quipId });
