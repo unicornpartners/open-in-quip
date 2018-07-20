6a93ece4c1a114216bd72cb023f4d246afb1f6eb
Matt Y <letters@mattgy.net>
2018-07-20 12:27:36 -0400

    changed URL from quip.com to quip-amazon.com; modified MIT license to reflect second author

--- oiq.js
+++ oiq.js
@@ -1,11 +1,5 @@
-/* 
- * Copyright (C) 2016 Mark Elliot
- *
- * This software may be modified and distributed under the terms 
- * of the MIT license. See the LICENSE file for details.
- */
 chrome.browserAction.onClicked.addListener(function (tab) {
-  if (tab.url.match(/https?:\/\/[A-Za-z0-9]+\.?quip\.com/) != null) {
+  if (tab.url.match(/https?:\/\/[A-Za-z0-9]+\.?quip-amazon\.com/) != null) {
     var quipId = tab.url.split('/').pop();
     if (quipId.length == 12 || quipId.length == 13) {
       chrome.tabs.update(tab.id, { "url": "quip://" + quipId });
