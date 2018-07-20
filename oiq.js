fd8c2744dc51557abb73c372543777cffe79dab7
Matt Y <letters@mattgy.net>
2018-07-20 16:00:53 -0400

    fixed (for real this time) corp dubdomain support

--- oiq.js
+++ oiq.js
@@ -1,8 +1,7 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
   if (tab.url.match(/https:\/\/(corp\.)?quip-amazon\.com/) != null) {
     var quipId = tab.url.split('/').pop();
-    if (quipId.length == 12 || quipId.length == 13) {
-      chrome.tabs.update(tab.id, { "url": "quip://" + quipId });
-    }
+    chrome.tabs.update(tab.id, { "url": "quip://" + quipId });
   }
+  else console.log("Not a Quip Amazon link");
 });
