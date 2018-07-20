fcf453f50aa5a2f5892a6765b5c1dee45e586a12
Matt Y <letters@mattgy.net>
2018-07-20 15:14:16 -0400

    added optional corp prefix to quip-amazon.com

--- oiq.js
+++ oiq.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
-  if (tab.url.match(/https?:\/\/quip-amazon\.com/) != null) {
+  if (tab.url.match(/https:\/\/(corp.)?quip-amazon\.com/) != null) {
     var quipId = tab.url.split('/').pop();
     if (quipId.length == 12 || quipId.length == 13) {
       chrome.tabs.update(tab.id, { "url": "quip://" + quipId });
